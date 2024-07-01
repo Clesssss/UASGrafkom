d196ced0d87accca02876c667a201a7089cac818
Zephylest <135185971+Zephylest@users.noreply.github.com>
2024-07-01 20:00:49 +0700

    Update free camera
    press (e) to switch camera modes

--- cleanUpMain.js
+++ cleanUpMain.js
@@ -3,6 +3,140 @@ import {OrbitControls} from "three/addons/controls/OrbitControls.js";
 import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 import  {PlayerController, ThirdPersonCamera, Player} from "./playerCleanUp.js";
+
+class FreeCamera {
+    constructor(camera) {
+        this.camera = camera;
+        this.velocity = new THREE.Vector3();
+        this.speed = 5;
+        this.rollSpeed = 0.05;
+        this.keys = {
+            "forward": false,
+            "backward": false,
+            "left": false,
+            "right": false,
+            "up": false,
+            "down": false,
+            "rollLeft": false,
+            "rollRight": false
+        };  
+
+        this.pitch = 0; // Keep track of the pitch angle
+        this.maxPitch = 1.8;
+        this.minPitch = -1.34;
+
+        // Initialize pitch and yaw as quaternions
+        this.pitchQuaternion = new THREE.Quaternion();
+        this.yawQuaternion = new THREE.Quaternion();
+        this.rollQuaternion = new THREE.Quaternion();
+
+        document.addEventListener("keydown", (e) => this.onKeyDown(e), false);
+        document.addEventListener("keyup", (e) => this.onKeyUp(e), false);
+        document.addEventListener("mousemove", (e) => this.onMouseMove(e), false);
+        document.addEventListener("mousedown", (e) => this.onMouseDown(e), false);
+        document.addEventListener("mouseup", (e) => this.onMouseUp(e), false);
+        document.addEventListener('pointerlockchange', this.onPointerLockChange.bind(this), false);
+        document.addEventListener('pointerlockerror', this.onPointerLockError.bind(this), false);
+    }
+
+    onKeyDown(event) {
+        switch (event.key) {
+            case 'w': this.keys["forward"] = true; break;
+            case 's': this.keys["backward"] = true; break;
+            case 'a': this.keys["left"] = true; break;
+            case 'd': this.keys["right"] = true; break;
+            case 'r': this.keys["up"] = true; break;
+            case 'f': this.keys["down"] = true; break;
+            case 'z': this.keys["rollLeft"] = true; break;
+            case 'x': this.keys["rollRight"] = true; break;
+        }
+    }
+
+    onKeyUp(event) {
+        switch (event.key) {
+            case 'w': this.keys["forward"] = false; break;
+            case 's': this.keys["backward"] = false; break;
+            case 'a': this.keys["left"] = false; break;
+            case 'd': this.keys["right"] = false; break;
+            case 'r': this.keys["up"] = false; break;
+            case 'f': this.keys["down"] = false; break;
+            case 'z': this.keys["rollLeft"] = false; break;
+            case 'x': this.keys["rollRight"] = false; break;
+        }
+    }
+
+    onMouseDown(event) {
+        this.requestPointerLock();
+    }
+
+    requestPointerLock() {
+        document.body.requestPointerLock();
+    }
+
+    onPointerLockChange() {
+        if (document.pointerLockElement === document.body) {
+            console.log('Pointer locked');
+        } else {
+            console.log('Pointer unlocked');
+        }
+    }
+
+    onPointerLockError() {
+        console.error('Pointer lock error');
+    }
+
+    onMouseMove(event) {
+        if (document.pointerLockElement === document.body) {
+            var pitchDelta = -event.movementY * 0.002; // pitch (up/down) control
+            const yawDelta = -event.movementX * 0.002; // yaw (left/right) control
+
+            if (this.pitch+pitchDelta < this.minPitch){
+                pitchDelta = this.minPitch - this.pitch
+            } else if (this.pitch+pitchDelta > this.maxPitch){
+                pitchDelta = this.maxPitch - this.pitch
+            }
+            this.pitch += pitchDelta;
+
+            // Update pitch and yaw quaternions
+            this.pitchQuaternion.setFromAxisAngle(new THREE.Vector3(1, 0, 0), pitchDelta);
+            this.yawQuaternion.setFromAxisAngle(new THREE.Vector3(0, 1, 0), yawDelta);
+
+            // Apply quaternions to the camera
+            this.camera.quaternion.multiplyQuaternions(this.yawQuaternion, this.camera.quaternion);
+            this.camera.quaternion.multiplyQuaternions(this.camera.quaternion, this.pitchQuaternion);
+
+            console.log(this.pitch);
+        }
+    }
+
+    update(delta) {
+        this.velocity.set(0, 0, 0);
+
+        if (this.keys["forward"]) this.velocity.z -= this.speed * delta;
+        if (this.keys["backward"]) this.velocity.z += this.speed * delta;
+        if (this.keys["left"]) this.velocity.x -= this.speed * delta;
+        if (this.keys["right"]) this.velocity.x += this.speed * delta;
+        if (this.keys["up"]) this.velocity.y += this.speed * delta;
+        if (this.keys["down"]) this.velocity.y -= this.speed * delta;
+
+        this.camera.translateX(this.velocity.x);
+        this.camera.translateY(this.velocity.y);
+        this.camera.translateZ(this.velocity.z);
+
+        if (this.keys["rollLeft"]) {
+            this.rollQuaternion.setFromAxisAngle(new THREE.Vector3(0, 0, 1), this.rollSpeed * delta*10);
+            this.camera.quaternion.multiplyQuaternions(this.camera.quaternion, this.rollQuaternion);
+        }
+        if (this.keys["rollRight"]) {
+            this.rollQuaternion.setFromAxisAngle(new THREE.Vector3(0, 0, 1), -this.rollSpeed * delta*10);
+            this.camera.quaternion.multiplyQuaternions(this.camera.quaternion, this.rollQuaternion);
+        }
+    }
+}
+
+export { FreeCamera };
+
+
 class Main{
     static init(){
         var canvasRef = document.getElementById("canvas");
@@ -105,29 +239,46 @@ class Main{
 
         var hemisphereLight = new THREE.HemisphereLight(0xB1e1ff, 0xB97a20, 0.1);
         this.scene.add(hemisphereLight);
-        var thirdPersonCamera = new ThirdPersonCamera(
-            this.camera, 
-            new THREE.Vector3(-5,5,0), 
-            new THREE.Vector3(0,0,0)
+
+        this.isFreeCamera = false;
+        this.freeCamera = new FreeCamera(this.camera);
+        this.thirdPersonCamera = new ThirdPersonCamera(
+            this.camera,
+            new THREE.Vector3(-5, 2, 0),
+            new THREE.Vector3(0, 0, 0)
         );
-        //thirdPersonCamera.setup(new THREE.Vector3(0,0,0));
+
+        document.addEventListener("keydown", (e) => this.onKeyDown(e), false);
 
         var controller = new PlayerController();
 
         this.player = new Player(
-            thirdPersonCamera,
-            controller, 
-            this.scene);
+            this.thirdPersonCamera,
+            controller,
+            this.scene
+        );
     }
     
-    static render (dt){
-        this.player.update(dt);
+    static onKeyDown(event) {
+        if (event.key === 'e') {
+            this.isFreeCamera = !this.isFreeCamera;
+            this.freeCamera.pitch = 0;
+        }
+    }
+
+    static render(dt) {
+        if (this.isFreeCamera) {
+            this.freeCamera.update(dt);
+        } else {
+            this.player.update(dt);
+        }
+
         this.renderer.render(this.scene, this.camera);
         this.scene.traverse(function (child) {
             if (child.isMesh && child.boundingBox) {
                 var box = new THREE.Box3().setFromObject(child);
                 if (box.intersectsBox(Main.player.boundingBox)) {
-                    console.log('Collision detected with', child.name);
+                    //console.log('Collision detected with', child.name);
                 }
             }
         });
